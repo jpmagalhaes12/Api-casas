c56cc89dd4f24d2de442de7d966e08935704323e
jpmagalhaes12 <j.magalhaes1450@gmail.com>
2023-11-12 16:13:44 -0300

    Colocando código no github

---
+++ src/controllers/DashboardController.js
@@ -0,0 +1,13 @@
+import House from "../models/House";
+
+class DashboardController{
+
+    async show(req, res){
+        return res.json({ok: true});
+
+    }
+
+
+}
+
+export default new DashboardController();

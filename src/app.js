75edfbd6bcc9d42868d3105ab4ddf55535f16478
jpmagalhaes12 <j.magalhaes1450@gmail.com>
2023-11-12 16:58:15 -0300

    inclusão do cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import path from 'path';
 import routes from './routes.js';
 
@@ -15,6 +16,7 @@ class App{
     }
 
     middlewares(){
+        this.server.use(cors());
         this.server.use(
             '/files',
             express.static(path.resolve(__dirname, '..', 'uploads'))

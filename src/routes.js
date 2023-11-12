01bbf363801b85e86e2f1ae25c9e91a0b7e44902
jpmagalhaes12 <j.magalhaes1450@gmail.com>
2023-11-12 17:49:05 -0300

    Adicionando a opção de realizar uma reserva

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from './config/upload.js'
-
 import SessionController from './controllers/SessionController';
 import HouseController from './controllers/HouseController';
 import DashboardController from './controllers/DashboardController.js';
+import ReserveController from './controllers/ReserveController.js';
 
 const routes = new Router();
 const upload = multer(uploadConfig);
@@ -17,5 +17,7 @@ routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.upda
 routes.delete('/houses', HouseController.destroy);
 
 routes.get('/dashboard', DashboardController.show);
+
+routes.post('/houses/:house_id/reserve', ReserveController.store);
 
 export default routes;
